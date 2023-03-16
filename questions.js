901f66171435fb66f5de3aac43597f746c8ac1dd
Marcel Dudek <dud.marcelek@gmail.com>
2023-03-16 18:46:40 +0100

    JS initial

---
+++ questions.js
@@ -0,0 +1,114 @@
+// array with questions
+
+let questions = [
+    {
+        numb: 1,
+        question: "Who was the master of Darth Sidious (Emperor Palpatine)?",
+        answer: "Darth Plagueis",
+        options: [
+            "Darth Tyranus",
+            "Darth Plagueis",
+            "Darth Vader",
+            "Darth Maul",
+        ]
+    },
+    {
+        numb: 2,
+        question: "What's the name of Boba Fett's ship?",
+        answer: "Slave I.",
+        options: [
+            "Slave I.",
+            "Millenium Falcon",
+            "Malevolence",
+            "Hound's tooth",
+        ]
+    },
+    {
+        numb: 3,
+        question: "Who was Anakin's Skywalker's padawan?",
+        answer: "Ashoka Tano",
+        options: [
+            "Barriss Offee",
+            "Cal Kestis",
+            "Ashoka Tano",
+            "Ezra Bridger",
+        ]
+    },
+    {
+        numb: 4,
+        question: "What planet did Anakin Skywalker loose his legs and arm on?",
+        answer: "Mustafar",
+        options: [
+            "Geonosis",
+            "Coruscant",
+            "Tatooine",
+            "Mustafar",
+        ]
+    },
+    {
+        numb: 5,
+        question: "What name did Obi-Wan Kenobi go by after the events of order 66?",
+        answer: "Ben Kenobi",
+        options: [
+            "Ben Kenobi",
+            "John Kenobi",
+            "Bob Kenobi",
+            "None of the above",
+        ]
+    },
+    {
+        numb: 6,
+        question: "Lightsabers are powered by what type of crystal?",
+        answer: "Kyber crystal",
+        options: [
+            "Light crystal",
+            "Nova crystal",
+            "Kyber crystal",
+            "Force crystal",
+        ]
+    },
+    {
+        numb: 7,
+        question: "What is the name of a clone that led the 501st Legion during the Clone Wars?",
+        answer: "Captain Rex",
+        options: [
+            "Commander Cody",
+            "Commander Wolf",
+            "Captain Gregor",
+            "Captain Rex",
+        ]
+    },
+    {
+        numb: 8,
+        question: "What species is Chewbacca?",
+        answer: "Wookie",
+        options: [
+            "Twi'lek",
+            "Wookie",
+            "Trandoshan",
+            "None of the above",
+        ]
+    },
+    {
+        numb: 9,
+        question: "What is the name of the female member of the Jedi High Council who is of the same species as Yoda?",
+        answer: "Yaddle",
+        options: [
+            "Yaddle",
+            "Grogu",
+            "Ki-Adi-Mundi",
+            "Shaak Ti",
+        ]
+    },
+    {
+        numb: 10,
+        question: "What was the name of the creature that Boba Fett fell into?",
+        answer: "Sarlacc",
+        options: [
+            "Acklay",
+            "Rancor",
+            "Sarlacc",
+            "Mythosaur",
+        ]
+    },
+];
--- script.js
+++ script.js
@@ -1,1 +1,54 @@
 // alert('Hello Star Wars fans! Let\'s test your SW knowledge');
+
+//all required elements
+
+const start_btn = document.querySelector(".start_btn button");
+const info_box = document.querySelector(".info_box");
+const quit_btn = info_box.querySelector(".buttons .quit");
+const continue_btn = info_box.querySelector(".buttons .restart");
+const quiz_box = document.querySelector(".quiz_box");
+
+// If Start Quiz button clicked
+
+start_btn.onclick = function() {
+    info_box.classList.add("activeInfo"); //show the info box
+}
+
+// If quit button clicked
+
+quit_btn.onclick = function() {
+    info_box.classList.remove("activeInfo"); //hide the info box
+}
+
+// If continue button clicked
+
+continue_btn.onclick = function() {
+    info_box.classList.remove("activeInfo"); //hide info box 
+    quiz_box.classList.add("activeQuiz"); //show the quiz box
+    showQuestions(0);
+}
+
+let que_count = 0;
+
+const next_btn = document.querySelector(".next_btn");
+
+// If next button clicked
+
+next_btn.onclick = function() {
+    que_count++;
+    showQuestions(que_count);
+}
+
+//getting questions and options from array
+
+function showQuestions(index) {
+    const que_text = document.querySelector(".que_text");
+    const option_list = document.querySelector(".option_list");
+    let que_tag = '<h5>' + questions[index].question + '</h5>';
+    let option_tag = '<div class="option">' + questions[index].options[0] + '<span></span></div>'
+                     + '<div class="option">' + questions[index].options[1] + '<span></span></div>'
+                     + '<div class="option">' + questions[index].options[2] + '<span></span></div>'
+                     + '<div class="option">' + questions[index].options[3] + '<span></span></div>';
+    que_text.innerHTML = que_tag;
+    option_list.innerHTML = option_tag;
+}
