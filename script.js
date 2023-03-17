22fb1a5167d0e5a9979e99aa36bdd5b2e9744bf5
Marcel Dudek <dud.marcelek@gmail.com>
2023-03-17 15:01:42 +0100

    beta version finished

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// alert('Hello Star Wars fans! Let\'s test your SW knowledge');
+alert('Hello Star Wars fans! Let\'s test your SW knowledge');
 
 //all required elements
 
@@ -32,8 +32,27 @@ continue_btn.onclick = function() {
 
 let que_count = 0;
 let que_numb = 1;
+let user_score = 0;
 
 const next_btn = document.querySelector(".next_btn");
+const result_box = document.querySelector(".result_box");
+const restart_quiz = result_box.querySelector(".buttons .restart");
+const quit_quiz = result_box.querySelector(".buttons .quit");
+
+restart_quiz.onclick = function() {
+    quiz_box.classList.add("activeQuiz");
+    result_box.classList.remove("activeResult");
+    let que_count = 0;
+    let que_numb = 1;
+    let user_score = 0;
+    showQuestions(que_count);
+    questionCount(que_numb);
+    next_btn.style.display = "none";
+}
+
+quit_quiz.onclick = function() {
+    window.location.reload();
+}
 
 // If next button clicked
 
@@ -42,9 +61,11 @@ next_btn.onclick = function() {
         que_count++;
         que_numb++;
         showQuestions(que_count);
-        questionCount(que_numb)
+        questionCount(que_numb);
+        next_btn.style.display = "none";
     } else {
         console.log('Questions completed');
+        showResultBox();
     }
 }
 
@@ -69,10 +90,12 @@ let tickIcon = '<div class="icon tick"><i class="fa fa-check"></i></div>';
 let crossIcon = '<div class="icon cross"><i class="fa fa-times"></i></div>';
 
 function optionSelected(answer) {
+    console.log(user_score)
     let userAns = answer.textContent;
     let correctAns = questions[que_count].answer;
     let allOptions = option_list.children.length;
     if(userAns === correctAns) {
+        user_score += 1;
         answer.classList.add("selected");
         console.log("Answer is correct");
         answer.insertAdjacentHTML("beforeend", tickIcon);
@@ -86,6 +109,24 @@ function optionSelected(answer) {
 
     for (let i = 0; i < allOptions; i++) {
         option_list.children[i].classList.add("disabled");
+    }
+    next_btn.style.display = "block";
+}
+
+function showResultBox() {
+    info_box.classList.remove("activeInfo"); //hide hte info box
+    quiz_box.classList.remove("activeQuiz"); //hide the quiz box
+    result_box.classList.add("activeResult"); //show the result box
+    const scoreText = result_box.querySelector(".score_text");
+    if (user_score > 7) {
+        let scoreTag = '<span>Got you correctly<br><p>'+ user_score +'</p> out of <p>'+ questions.length +'</p><br>questions!<br>Strong the force with you is!</span>';
+        scoreText.innerHTML = scoreTag;
+    } else if (user_score < 7 && user_score > 4) {
+        let scoreTag = '<span>Got you correctly<br><p>'+ user_score +'</p> out of <p>'+ questions.length +'</p><br>questions.<br>Well have you done!</span>';
+        scoreText.innerHTML = scoreTag;
+    } else {
+        let scoreTag = '<span>Got you only<br><p>'+ user_score +'</p> out of <p>'+ questions.length +'</p><br>questions right...<br>Again try you must!</span>';
+        scoreText.innerHTML = scoreTag; 
     }
 }
 
